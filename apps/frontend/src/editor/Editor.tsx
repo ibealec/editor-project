7dde649b464d8e8089e0e2ef95f1336c60495f43
Alec Branaa <ibealec@gmail.com>
2022-01-26 18:02:15 -0800

    feat: Deserialize input that is copy pasted

--- apps/frontend/src/editor/Editor.tsx
+++ apps/frontend/src/editor/Editor.tsx
@@ -1,45 +1,52 @@
 // @refresh reset // Fixes hot refresh errors in development https://github.com/ianstormtaylor/slate/issues/3477
 
-import React, { useCallback, useMemo, useState } from 'react'
-import { createEditor, Descendant, BaseEditor } from 'slate'
-import { withHistory, HistoryEditor } from 'slate-history'
-import { handleHotkeys } from './helpers'
-
-import { Editable, withReact, Slate, ReactEditor } from 'slate-react'
-import { EditorToolbar } from './EditorToolbar'
-import { CustomElement } from './CustomElement'
-import { CustomLeaf, CustomText } from './CustomLeaf'
+import React, { useCallback, useMemo, useState } from 'react';
+import { BaseEditor, createEditor, Descendant } from 'slate';
+import { HistoryEditor, withHistory } from 'slate-history';
+import { Editable, ReactEditor, Slate, withReact } from 'slate-react';
+import { CustomElement } from './CustomElement';
+import { CustomLeaf, CustomText } from './CustomLeaf';
+import { EditorToolbar } from './EditorToolbar';
+import { handleHotkeys, withHtml } from './helpers';
 
 // Slate suggests overwriting the module to include the ReactEditor, Custom Elements & Text
 // https://docs.slatejs.org/concepts/12-typescript
 declare module 'slate' {
   interface CustomTypes {
-    Editor: BaseEditor & ReactEditor & HistoryEditor
-    Element: CustomElement
-    Text: CustomText
+    Editor: BaseEditor & ReactEditor & HistoryEditor;
+    Element: CustomElement;
+    Text: CustomText;
   }
 }
 
 interface EditorProps {
-  initialValue?: Descendant[]
-  placeholder?: string
+  initialValue?: Descendant[];
+  placeholder?: string;
 }
 
-export const Editor: React.FC<EditorProps> = ({ initialValue = [], placeholder }) => {
-  const [value, setValue] = useState<Array<Descendant>>(initialValue)
-  const renderElement = useCallback(props => <CustomElement {...props} />, [])
-  const renderLeaf = useCallback(props => <CustomLeaf {...props} />, [])
-  const editor = useMemo(() => withHistory(withReact(createEditor())), [])
+export const Editor: React.FC<EditorProps> = ({
+  initialValue = [],
+  placeholder,
+}) => {
+  const [value, setValue] = useState<Array<Descendant>>(initialValue);
+  const renderElement = useCallback(
+    (props) => <CustomElement {...props} />,
+    []
+  );
+  const renderLeaf = useCallback((props) => <CustomLeaf {...props} />, []);
+  const editor = useMemo(
+    () => withHtml(withHistory(withReact(createEditor()))),
+    []
+  );
 
   return (
-    <Slate editor={editor} value={value} onChange={value => setValue(value)}>
+    <Slate editor={editor} value={value} onChange={(value) => setValue(value)}>
       <EditorToolbar />
       <Editable
         renderElement={renderElement}
         renderLeaf={renderLeaf}
         placeholder={placeholder}
         onKeyDown={handleHotkeys(editor)}
-
         // The dev server injects extra values to the editr and the console complains
         // so we override them here to remove the message
         autoCapitalize="false"
@@ -47,5 +54,5 @@ export const Editor: React.FC<EditorProps> = ({ initialValue = [], placeholder }
         spellCheck="false"
       />
     </Slate>
-  )
-}
+  );
+};
--- apps/frontend/src/editor/helpers.ts
+++ apps/frontend/src/editor/helpers.ts
@@ -1,70 +1,204 @@
-import { Editor, Transforms, Element as SlateElement } from 'slate'
-import isHotkey from 'is-hotkey'
-import { KeyboardEvent } from 'react'
-import { CustomElementType } from './CustomElement'
-import { CustomText } from './CustomLeaf'
+import isHotkey from 'is-hotkey';
+import { KeyboardEvent } from 'react';
+import { BaseEditor, Editor, Element as SlateElement, Transforms } from 'slate';
+import { HistoryEditor } from 'slate-history';
+import { jsx } from 'slate-hyperscript';
+import { ReactEditor } from 'slate-react';
+import { CustomElement, CustomElementType } from './CustomElement';
+import { CustomText } from './CustomLeaf';
 
-const LIST_TYPES = ['numbered-list', 'bulleted-list']
+const LIST_TYPES = ['numbered-list', 'bulleted-list'];
 
-export const toggleBlock = (editor: Editor, format: CustomElementType): void => {
-  const isActive = isBlockActive(editor, format)
-  const isList = LIST_TYPES.includes(format)
+const ELEMENT_TAGS = {
+  A: (el) => ({ type: 'link', url: el.getAttribute('href') }),
+  BLOCKQUOTE: () => ({ type: 'quote' }),
+  H1: () => ({ type: 'heading-one' }),
+  H2: () => ({ type: 'heading-two' }),
+  H3: () => ({ type: 'heading-three' }),
+  H4: () => ({ type: 'heading-four' }),
+  H5: () => ({ type: 'heading-five' }),
+  H6: () => ({ type: 'heading-six' }),
+  IMG: (el) => ({ type: 'image', url: el.getAttribute('src') }),
+  LI: () => ({ type: 'list-item' }),
+  OL: () => ({ type: 'numbered-list' }),
+  P: () => ({ type: 'paragraph' }),
+  PRE: () => ({ type: 'code' }),
+  UL: () => ({ type: 'bulleted-list' }),
+};
+
+// COMPAT: `B` is omitted here because Google Docs uses `<b>` in weird ways.
+const TEXT_TAGS = {
+  CODE: () => ({ code: true }),
+  DEL: () => ({ strikethrough: true }),
+  EM: () => ({ italic: true }),
+  I: () => ({ italic: true }),
+  S: () => ({ strikethrough: true }),
+  STRONG: () => ({ bold: true }),
+  U: () => ({ underline: true }),
+};
+
+export const toggleBlock = (
+  editor: Editor,
+  format: CustomElementType
+): void => {
+  const isActive = isBlockActive(editor, format);
+  const isList = LIST_TYPES.includes(format);
 
   Transforms.unwrapNodes(editor, {
-    match: n =>
+    match: (n) =>
       LIST_TYPES.includes(
-        !Editor.isEditor(n) && SlateElement.isElement(n) && n.type as any // eslint-disable-line @typescript-eslint/no-explicit-any
+        !Editor.isEditor(n) && SlateElement.isElement(n) && (n.type as any) // eslint-disable-line @typescript-eslint/no-explicit-any
       ),
     split: true,
-  })
+  });
   const newProperties: Partial<SlateElement> = {
-    type: isActive ? CustomElementType.paragraph : isList ? CustomElementType.listItem : format,
-  }
-  Transforms.setNodes(editor, newProperties)
+    type: isActive
+      ? CustomElementType.paragraph
+      : isList
+      ? CustomElementType.listItem
+      : format,
+  };
+  Transforms.setNodes(editor, newProperties);
 
   if (!isActive && isList) {
-    const block = { type: format, children: [] }
-    Transforms.wrapNodes(editor, block)
+    const block = { type: format, children: [] };
+    Transforms.wrapNodes(editor, block);
   }
-}
+};
 
 export const toggleMark = (editor: Editor, format: keyof CustomText): void => {
-  const isActive = isMarkActive(editor, format)
+  const isActive = isMarkActive(editor, format);
 
   if (isActive) {
-    Editor.removeMark(editor, format)
+    Editor.removeMark(editor, format);
   } else {
-    Editor.addMark(editor, format, true)
+    Editor.addMark(editor, format, true);
   }
-}
+};
 
-export const isBlockActive = (editor: Editor, format: CustomElementType): boolean => {
+export const isBlockActive = (
+  editor: Editor,
+  format: CustomElementType
+): boolean => {
   const [match] = Editor.nodes(editor, {
-    match: n =>
+    match: (n) =>
       !Editor.isEditor(n) && SlateElement.isElement(n) && n.type === format,
-  })
+  });
 
-  return !!match
-}
+  return !!match;
+};
 
-export const isMarkActive = (editor: Editor, format: keyof CustomText): boolean => {
-  const marks = Editor.marks(editor)
-  return marks ? format in marks === true : false
-}
+export const isMarkActive = (
+  editor: Editor,
+  format: keyof CustomText
+): boolean => {
+  const marks = Editor.marks(editor);
+  return marks ? format in marks === true : false;
+};
 
 const HOTKEYS: Record<string, keyof CustomText> = {
   'mod+b': 'bold',
   'mod+i': 'italic',
   'mod+u': 'underline',
   'mod+`': 'code',
-}
+};
 
-export const handleHotkeys = (editor: Editor) => (event: KeyboardEvent<HTMLDivElement>): void => {
-  for (const hotkey in HOTKEYS) {
-    if (isHotkey(hotkey, event)) {
-      event.preventDefault()
-      const mark = HOTKEYS[hotkey]
-      toggleMark(editor, mark)
+export const handleHotkeys =
+  (editor: Editor) =>
+  (event: KeyboardEvent<HTMLDivElement>): void => {
+    for (const hotkey in HOTKEYS) {
+      if (isHotkey(hotkey, event)) {
+        event.preventDefault();
+        const mark = HOTKEYS[hotkey];
+        toggleMark(editor, mark);
+      }
     }
+  };
+
+// Newly pasted content needs to be deserialized into data that Slate can understand.
+// Though there are plugins that help with this, we've opted to use code from Slates examples:
+// https://github.com/ianstormtaylor/slate/blob/f1b7d18f43913474617df02f747afa0e78154d85/site/examples/paste-html.tsx
+// The deserialization is straightforward enough to not warrent using Platejs or any other library.
+
+export const deserialize: string | CustomElement | null = (el: ChildNode) => {
+  if (el.nodeType === 3) {
+    return el.textContent;
+  } else if (el.nodeType !== 1) {
+    return null;
   }
-}
+
+  // This ensures that <br> elements get deserialized to empty paragraphs, rather than just new lines.
+  //  This means the editor will recognize that it's a new paragraph, and place the cursor at the end of it.
+  //  https://github.com/ianstormtaylor/slate/issues/3457#issuecomment-577395255
+
+  if (el.nodeName === 'BR') {
+    return jsx('element', { type: 'paragraph' }, [{ text: '' }]);
+  }
+
+  const { nodeName } = el;
+  let parent = el;
+
+  if (
+    nodeName === 'PRE' &&
+    el.childNodes[0] &&
+    el.childNodes[0].nodeName === 'CODE'
+  ) {
+    parent = el.childNodes[0];
+  }
+  let children: ChildNode[] = Array.from(parent.childNodes)
+    .map(deserialize)
+    .flat();
+
+  if (children.length === 0) {
+    children = [{ text: '' }];
+  }
+
+  if (el.nodeName === 'BODY') {
+    return jsx('fragment', {}, children);
+  }
+
+  if (ELEMENT_TAGS[nodeName]) {
+    const attrs = ELEMENT_TAGS[nodeName](el);
+    return jsx('element', attrs, children);
+  }
+
+  if (TEXT_TAGS[nodeName]) {
+    const attrs = TEXT_TAGS[nodeName](el);
+    return children.map((child: ChildNode) => jsx('text', attrs, child));
+  }
+
+  return children;
+};
+
+export const withHtml = (editor: ReactEditor & HistoryEditor & BaseEditor) => {
+  const { insertData, isInline, isVoid } = editor;
+
+  editor.isInline = (element) => {
+    return element.type === 'link' ? true : isInline(element);
+  };
+
+  editor.isVoid = (element) => {
+    return element.type === 'image' ? true : isVoid(element);
+  };
+
+  editor.insertData = (data) => {
+    const html = data.getData('text/html');
+
+    if (html) {
+      const parsed = new DOMParser().parseFromString(html, 'text/html');
+      const fragment = deserialize(parsed.body);
+      console.log('F', fragment);
+      try {
+        Transforms.insertFragment(editor, fragment);
+      } catch (e) {
+        console.log('E', e);
+      }
+
+      return;
+    }
+
+    insertData(data);
+  };
+
+  return editor;
+};
